9b5e15d718b36ec3bdbfd6c0380700b7969a9f6f
OrpheasK <45125993+OrpheasK@users.noreply.github.com>
2020-11-12 14:14:36 +0200

    Add files via upload

--- midi21txt.py
+++ midi21txt.py
@@ -1,6 +1,7 @@
 from music21 import converter, corpus, instrument, midi, note, chord, pitch, stream, tempo
 import csv
 from operator import itemgetter
+import os
 
 #https://www.kaggle.com/wfaria/midi-music-data-extraction-using-music21
 def open_midi(midi_path, remove_tracks):
@@ -17,7 +18,6 @@ def open_midi(midi_path, remove_tracks):
 
     return midi.translate.midiFileToStream(mf)
     
-base_midi = open_midi("C:/Users/Papias/Desktop/thesis/copy/midi/midi.mid", True) #open and remove drums
 # temp_midi_chords = open_midi("C:/Users/Papias/Desktop/thesis/copy/midi/midi.mid",True).chordify()
 # base_midi = stream.Score()
 # base_midi.insert(0, temp_midi_chords)
@@ -26,22 +26,27 @@ base_midi = open_midi("C:/Users/Papias/Desktop/thesis/copy/midi/midi.mid", True)
 # for v in base_midi.voices:
 	# for n in v.notes:
 		# a.insert(n.offset, n)
+
+def textpr(b_midi, fname):		
+	#based on https://colab.research.google.com/github/cpmpercussion/creative-prediction/blob/master/notebooks/3-zeldic-musical-RNN.ipynb
+	stream_list = []
+
+	for i in range(len(b_midi.parts)):
+		for element in b_midi.parts[i].flat:
+			if isinstance(element, note.Note):
+				stream_list.append([float(element.offset), float(element.quarterLength), element.pitch.midi])
+				# a.insert(element.offset, element)
+			elif isinstance(element, chord.Chord):
+				for nt in element.pitches:
+					stream_list.append([float(element.offset), float(element.quarterLength), nt.midi])
+
+	stream_list = sorted(stream_list, key=itemgetter(0))
+
+	with open("C:/Users/Papias/Desktop/thesis/copy/midi/midi21txt/" + fname[:-4] + ".txt", "w", newline="") as f:
+		writer = csv.writer(f)
+		writer.writerows(stream_list)
 	
-#based on https://colab.research.google.com/github/cpmpercussion/creative-prediction/blob/master/notebooks/3-zeldic-musical-RNN.ipynb
-stream_list = []
-
-for i in range(len(base_midi.parts)):
-	for element in base_midi.parts[i].flat:
-		if isinstance(element, note.Note):
-			stream_list.append([float(element.offset), float(element.quarterLength), element.pitch.midi])
-			a.insert(element.offset, element)
-		elif isinstance(element, chord.Chord):
-			for nt in element.pitches:
-				stream_list.append([float(element.offset), float(element.quarterLength), nt.midi])
-
-stream_list = sorted(stream_list, key=itemgetter(0))
-
-with open("C:/Users/Papias/Desktop/thesis/copy/midi/mademidiall.txt", "w", newline="") as f:
-    writer = csv.writer(f)
-    writer.writerows(stream_list)
-	
+for directory, subdirectories, files in os.walk('C:/Users/Papias/Desktop/thesis/copy/midi/Rock_Cleansed'):
+    for file in files:
+        base_midi = open_midi(os.path.abspath(os.path.join(directory, file)), True) #open and remove drums
+        textpr(base_midi, file)
--- npztomidi.py
+++ npztomidi.py
@@ -1,8 +1,16 @@
 import pypianoroll
 import matplotlib
 from matplotlib import pyplot
+import os
 
-npzf = pypianoroll.load('C:/Users/Papias/Desktop/thesis/copy/Rock_Cleansed/TRABVVN12903CB6445/d809d7337042605a186eef7a81fa49f6.npz')
+root_dir = 'C:/Users/Papias/Desktop/thesis/copy/Rock_Cleansed'
+
+for directory, subdirectories, files in os.walk(root_dir):
+    for file in files:
+        # print (os.path.abspath(os.path.join(directory, file)))
+        npzf = pypianoroll.load(os.path.abspath(os.path.join(directory, file)))
+        pypianoroll.write(npzf, 'C:/Users/Papias/Desktop/thesis/copy/midi/Rock_Cleansed/' + directory[-18:] + '.mid')
+		
 # fig, ax = pypianoroll.plot_multitrack(npzf, 'C:/Users/Papias/Desktop/thesis/copy/midi', mode='separate', track_label='name', preset='default', cmaps=None, xtick='auto', ytick='octave', xticklabel=True, yticklabel='auto', tick_loc=None, tick_direction='in', label='both', grid='both', grid_linestyle=':', grid_linewidth=0.5)
 # pyplot.show()
-pypianoroll.write(npzf, 'C:/Users/Papias/Desktop/thesis/copy/midi/altr2.mid')
+
